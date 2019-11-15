1558d2c5f717ab154e4342d289010e79f3da16f7
wonderfulMan <1259243840@qq.com>
2019-11-15 17:50:08 +0800

    eslint update

--- dist/helper/eslint.js
+++ dist/helper/eslint.js
@@ -10,9 +10,26 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
+var __read = (this && this.__read) || function (o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var path = require("path");
 var path_1 = require("./path");
+var check_1 = require("../helper/check");
 function getEslintParser(customConfig) {
     var parser = 'babel-eslint';
     if (customConfig.eslintConfig.parser) {
@@ -92,18 +109,27 @@ function getRules(customConfig) {
 function getSettings(customConfig) {
     var settings = {};
     var extensions = [".js", ".jsx"];
+    var alias = {};
     if (customConfig.frame === 'react') {
         customConfig.typescript && extensions.push('.tsx');
     }
     customConfig.frame === 'vue' && extensions.push('.vue');
+    if (customConfig.alias) {
+        var size = check_1.getObjectSize(customConfig.alias);
+        if (size) {
+            Object.entries(customConfig.alias)
+                .forEach(function (_a) {
+                var _b = __read(_a, 2), key = _b[0], value = _b[1];
+                return (alias[key] = path.join(path_1.default.WORK_DIR_PATH(), value));
+            });
+        }
+    }
     settings["import/resolver"] = {
         "webpack": {
             "config": {
                 "resolve": {
                     extensions: extensions,
-                    alias: {
-                        'react-dom': require.resolve('@hot-loader/react-dom')
-                    }
+                    alias: __assign({ 'react-dom': require.resolve('@hot-loader/react-dom') }, alias)
                 }
             }
         }
