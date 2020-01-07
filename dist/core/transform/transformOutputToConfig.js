05cf6f8614f447694989a18c811f94aa8fa90820
wonderfulMan <1259243840@qq.com>
2020-01-07 15:03:47 +0800

    优化脚手架

--- dist/core/transform/transformOutputToConfig.js
+++ dist/core/transform/transformOutputToConfig.js
@@ -2,14 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var path_1 = require("../../helper/path");
 function transformOutputToConfig(webpackConfig, customConfig) {
+    var _a, _b, _c;
     var selectHashType = webpackConfig.mode === 'development' ? 'hash' : 'contenthash';
-    var publicPath = webpackConfig.mode === 'development' ? '/' : customConfig.publicPath || '../';
     webpackConfig.output = {
         path: path_1.default.BUIL_DIR_PATH,
         filename: "[name]/app-[" + selectHashType + ":8].bundle.js",
-        publicPath: publicPath,
         chunkFilename: "[name]/[name]-chunk-[" + selectHashType + ":8].bundle.js",
-        pathinfo: webpackConfig.mode === 'development'
+        pathinfo: webpackConfig.mode === 'development',
+        library: (_a = customConfig.output) === null || _a === void 0 ? void 0 : _a.library,
+        libraryTarget: (_b = customConfig.output) === null || _b === void 0 ? void 0 : _b.libraryTarget,
+        publicPath: ((_c = customConfig.output) === null || _c === void 0 ? void 0 : _c.publicPath) || webpackConfig.mode === 'development' ? '/' : '../'
     };
 }
 exports.transformOutputToConfig = transformOutputToConfig;
